88ff74584c3e9b8fa02673768d3d8911c4cc65df
Zaidi <zaidiahsan.r@gmail.com>
2023-09-08 22:06:41 -0500

    projects responsive

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -8,6 +8,8 @@ const projectsData = [
       description: "Project 1 description",
       image: "/images/projects/1.png",
       tag: ["All", "Web"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
     {
       id: 2,
@@ -15,6 +17,8 @@ const projectsData = [
       description: "Project 2 description",
       image: "/images/projects/2.png",
       tag: ["All", "Web"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
     {
       id: 3,
@@ -22,6 +26,8 @@ const projectsData = [
       description: "Project 3 description",
       image: "/images/projects/3.png",
       tag: ["All", "Web"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
     {
       id: 4,
@@ -29,6 +35,8 @@ const projectsData = [
       description: "Project 4 description",
       image: "/images/projects/4.png",
       tag: ["All", "Mobile"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
     {
       id: 5,
@@ -36,6 +44,8 @@ const projectsData = [
       description: "Authentication and CRUD operations",
       image: "/images/projects/5.png",
       tag: ["All", "Web"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
     {
       id: 6,
@@ -43,22 +53,28 @@ const projectsData = [
       description: "Project 5 description",
       image: "/images/projects/6.png",
       tag: ["All", "Web"],
+      gitUrl: '/',
+      previewUrl: '/'
     },
 ];
 
 const ProjectsSection = () => {
     return (
         <>
-            <h2>My Projects</h2>
-            <div>
+            <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+                My Projects
+            </h2>
+            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
                 {projectsData.map((project) => 
                     <ProjectCard 
                         key={project.id} 
                         title={project.title}
                         description={project.description}
                         imgUrl={project.image}
+                        gitUrl={project.gitUrl}
+                        previewUrl={project.previewUrl}
                     />
-                )};
+                )}
             </div>
         </>
     )
