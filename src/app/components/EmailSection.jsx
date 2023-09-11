2dce851211f3089eb4e75293f4f28bb1b519cbdf
Zaidi <zaidiahsan.r@gmail.com>
2023-09-10 20:07:23 -0500

    email format

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -11,9 +11,7 @@ const EmailSection = () => {
       <div className="z-100">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          I'm actively exploring new opportunities, and you're welcome to reach out anytime. Whether you have questions or simply want to connect, I'll try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com/Ahsan-Zaidi">
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
                             1000,
                             'Back-end Developer',
                             1000,
-                            'React Developer',
+                            'Web Designer',
                             1000
                         ]}
                         wrapper="span"
@@ -35,10 +35,14 @@ const HeroSection = () => {
                     </p>
                     <div>
                         <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
-                            Reach Out
+                            <a href='#contact'>
+                                Reach Out
+                            </a>
                         </button>
                         <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
-                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                                Download CV
+                            </span>
                         </button>
                     </div>
                 </div>
