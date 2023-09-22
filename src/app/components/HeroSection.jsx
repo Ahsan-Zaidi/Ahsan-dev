1cd43fa545d62b31ce31b0aaba640db92867f4c1
Zaidi <zaidiahsan.r@gmail.com>
2023-09-22 12:18:08 -0500

    title change and resume addition

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,9 +46,11 @@ const HeroSection = () => {
                             </a>
                         </button>
                         <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
-                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
-                                Download CV
-                            </span>
+                            <a href="https://drive.google.com/file/d/1Tn3Cslw0J0Xw6rPlzLquBvoECeM1qEQK/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                                    Download CV
+                                </span>
+                            </a>
                         </button>
                     </div>
                 </motion.div>
--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,6 @@ import Footer from './components/Footer';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Head>
-        <title>Ahsan-dev</title>
-      </Head>
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
