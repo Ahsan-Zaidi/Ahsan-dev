75f08cc26d7cee0d2475e0597dc1eeeb7465425d
Zaidi <zaidiahsan.r@gmail.com>
2023-09-22 20:27:32 -0500

    remove client comments

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useTransition, useState } from 'react';
 import Image from 'next/image';
 import TabButton from './TabButton';
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useRef } from 'react';
+import React from 'react';
 import ProjectCard from './ProjectCard';
 
 const projectsData = [
