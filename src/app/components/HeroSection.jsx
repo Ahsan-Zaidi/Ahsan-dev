868834a18fbe3e99c12856b83d9314ea1b35a8d5
Zaidi <zaidiahsan.r@gmail.com>
2023-10-12 12:24:19 -0500

    resume refactor

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,79 +1,82 @@
-"use client";
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
 
 const HeroSection = () => {
-    return (
-        <section className='lg:py-16'>
-            <div className='grid grid-cols-1 sm:grid-cols-12'>
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5}}
-                    className='col-span-7 place-self-center text-center sm:text-left'
-                >
-                    <h1 className='text-white mb-8 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
-                            Hello, I'm{" "}
-                        </span>
-                        <br></br>
-                        <TypeAnimation
-                        sequence={[
-                            // Same substring at the start will only be typed out once, initially
-                            'Ahsan',
-                            1000, // wait 1s before replacing "Mice" with "Hamsters"
-                            'Front-end Developer',
-                            1000,
-                            'Back-end Developer',
-                            1000,
-                            'Web Designer',
-                            1000
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={Infinity}
-                        />
-                    </h1>
-                    <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-                        "When a program works, it is beautiful. The art of programming is the skill of controlling complexity." 
-                    </p>
-                    <div>
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
-                            <a href='#contact'>
-                                Reach Out
-                            </a>
-                        </button>
-                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
-                            <a href="https://drive.google.com/file/d/1Tn3Cslw0J0Xw6rPlzLquBvoECeM1qEQK/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
-                                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
-                                    Download CV
-                                </span>
-                            </a>
-                        </button>
-                    </div>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5}}
-                    className='col-span-5 place-self-center mt-4 lg:mt-0'
-                >
-                    <div className='rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                        <Image
-                            src='/images/programmer.png'
-                            alt='Hero image'
-                            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                            width={300}
-                            height={300}
-                            priority={true}
-                        />
-                    </div>
-                </motion.div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
+          <h1 className="text-white mb-8 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{' '}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Ahsan',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Front-end Developer',
+                1000,
+                'Back-end Developer',
+                1000,
+                'Web Designer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            "When a program works, it is beautiful. The art of programming is
+            the skill of controlling complexity."
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              <a href="#contact">Reach Out</a>
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <a
+                href="https://drive.google.com/file/d/1D6ijZPOy49MO_TBhsS62qS3luz1BpPk1/view?usp=drive_link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                  Download CV
+                </span>
+              </a>
+            </button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-5 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/programmer.png"
+              alt="Hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+              priority={true}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
